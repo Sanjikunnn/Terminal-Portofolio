7433bc8b734753ce1b768a9665bcb772015638dd
Sanjikunnn <fmiqbal66@gmail.com>
2025-03-20 23:35:04 +0700

    Fix vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "./", // biar path relatif
-  ssr: false, // Nonaktifkan SSR jika tidak diperlukan
-
 })
