d5aa46a4a71983e6a9669febac8f8a30f14d1e5a
Sanjikunnn <fmiqbal66@gmail.com>
2025-03-20 21:30:23 +0700

    Menyimpan perubahan sebelum pull

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Tambahkan ini biar path relatif
+  base: "./", // biar path relatif
 })
